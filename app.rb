a34cfd9bd9f54b76205e23d46488c988297a5adc
SiobhanGi <siobhan.goggins@gmail.com>
2018-05-08 14:22:21 +0100

    failing test for bookmark list feature

--- app.rb
+++
@@ -1,4 +0,0 @@
-require 'sinatra/base'
-
-class Bookmark < Sinatra::Base
-end
---
+++ spec/features/features_spec.rb
@@ -0,0 +1,12 @@
+RSpec.feature BookmarkManager do
+  scenario 'intro to homepage' do
+    visit '/'
+    expect(page).to have_content 'Bookmark Manager Homepage'
+  end
+  scenario 'view list of bookmarks' do
+    visit '/'
+    expect(page).to have_content 'https://hackernoon.com'
+    expect(page).to have_content 'https://google.com'
+    expect(page).to have_content 'https://yahoo.de'
+  end
+end
